6a762101874fd623ad4eb68b9056f50920fec1d0
backend-ac <backend@astana-creative.kz>
2023-01-13 19:02:50 +0600

    js

--- js/main.js
+++ js/main.js
@@ -141,6 +141,8 @@ d3.csv("./data/AIU-All-Women-Dataset-csv.csv", d => {
 		d3.select(this).style("stroke", "#333")
     		.style("stroke-width", 2);
 
+    	d3.select(this).attr('class', 'active');
+
     	console.log(mainData);
 	});
 
