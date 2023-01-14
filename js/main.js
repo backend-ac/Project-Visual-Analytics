885091bac99683f94cc4d2ab03b5b4d6ccf43ff3
backend-ac <backend@astana-creative.kz>
2023-01-14 22:27:27 +0600

    js

--- js/main.js
+++ js/main.js
@@ -382,4 +382,58 @@ d3.csv("./data/AIU-All-Women-Dataset-csv.csv", d => {
 		.attr("dx", "10")
 		.attr("dy", "10");
 
+
+
+
+	/*--------- Map -------*/
+
+
+	// var svg = d3.select("svg"),
+	// 	width = +svg.attr("width"),
+	// 	height = +svg.attr("height");
+
+	var mapSvg = d3.select('#groupBar')
+		.append("svg")
+    	.attr("viewBox", [0, 0, width, height]);
+
+
+	var path = d3.geoPath();
+	var projection = d3.geoMercator()
+		.scale(120)
+		.center([0,20])
+		.translate([width / 2, height / 2]);
+
+
+	var data = d3.map();
+	var colorScale = d3.scaleThreshold()
+		.domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+		.range(d3.schemeBlues[7]);
+
+
+	d3.queue()
+		.defer(d3.json, "https://backend-ac.github.io/Project-Visual-Analytics/data/world.json")
+		// .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
+		.await(ready);
+
+	function ready(error, topo) {
+		mapSvg.append("g")
+		.selectAll("path")
+		.data(topo.features)
+		.enter()
+		.append("path")
+
+		.attr("d", d3.geoPath()
+			.projection(projection)
+			)
+
+		.attr("fill", function (d) {
+			// d.total = data.get(d.id) || 0;
+			d.total = 0;
+			return colorScale(d.total);
+		})
+		.style("stroke", "transparent")
+		.attr("class", function(d){ return "Country" } )
+		.style("opacity", .8)
+	}
+
 });
